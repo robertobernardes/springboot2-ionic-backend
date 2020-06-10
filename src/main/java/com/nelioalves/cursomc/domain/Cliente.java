1a9fd8dd0f1be4ec2c8e57c159d8e28ad1f53d23
Roberto Bernardes <roberto.bernardes@inema.ba.gov.br>
2020-06-10 20:27:24 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/nelioalves/cursomc/domain/Cliente.java
+++ src/main/java/com/nelioalves/cursomc/domain/Cliente.java
@@ -19,6 +19,7 @@ import com.nelioalves.cursomc.domain.enums.TipoCliente;
 
 @Entity
 public class Cliente implements Serializable {
+	
 	private static final long serialVersionUID = 1L;
 
 	@Id
@@ -36,6 +37,9 @@ public class Cliente implements Serializable {
 	@ElementCollection
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
+	
+	@OneToMany(mappedBy = "cliente")	
+	private List<Pedido> pedidos = new ArrayList<>();
 
 	public Cliente() {
 	}
@@ -105,6 +109,14 @@ public class Cliente implements Serializable {
 		this.telefones = telefones;
 	}
 
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -129,7 +141,4 @@ public class Cliente implements Serializable {
 			return false;
 		return true;
 	}
-
-
-
 }
